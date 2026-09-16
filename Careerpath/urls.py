098be845603ce814c540299c68e509644c4156f9
Nhu2804 <nhudnqse171733@fpt.edu.vn>
2026-09-16 15:03:21 +0700

    fix rpds-py render error

--- Careerpath/urls.py
+++ Careerpath/urls.py
@@ -17,7 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include, re_path
 from django.conf import settings
-from django.conf.urls.static import static
+from django.views.static import serve
 from main import views as main_views
 from core.views_admin import admin_dashboard
 from django.views.generic import RedirectView
@@ -35,6 +35,5 @@ urlpatterns = [
     path('trend/', include('trend.urls')),
     path('premium/', include('premium.urls')),
     path('oauth/', include('social_django.urls', namespace='social')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+]
